cfb5468251a6113cd12d77f8275eaf985a52c0c5
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-03-31 23:50:33 +0200

    menu done

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using System.Linq;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class SettingsMenu : MonoBehaviour
 
     public void Start()
     {
-        resolutions = Screen.resolutions;
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
@@ -33,6 +34,8 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+
+        Screen.fullScreen = true;
     }
 
     public void SetVolume(float volume)
